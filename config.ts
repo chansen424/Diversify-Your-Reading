d198d1e0cf6daf292a6d44e0684bc56487447bd0
Christopher Hansen <chansen424@yahoo.com>
2021-08-03 14:16:54 -0400

    Configure firebase hosting

---
+++ config.ts
@@ -0,0 +1,10 @@
+const firebaseConfig = {
+    apiKey: "AIzaSyCIorYsvZvnP8I29W0E1OPTY88hc-BFbks",
+    authDomain: "diversify-your-reading.firebaseapp.com",
+    projectId: "diversify-your-reading",
+    storageBucket: "diversify-your-reading.appspot.com",
+    messagingSenderId: "273274155737",
+    appId: "1:273274155737:web:2eb0a097865df19a55c4c8"
+};
+
+export default firebaseConfig;
