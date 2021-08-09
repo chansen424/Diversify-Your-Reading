acfc5b8c3588c964fcaa48f1af048267106238f4
Christopher Hansen <chansen424@yahoo.com>
2021-08-09 15:15:46 -0400

    feat: modal close button and overlay

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -1,12 +1,30 @@
 import styles from "./Modal.module.css";
+import { BsX } from "react-icons/bs";
 
 interface Showable {
   show: boolean;
+  onClose: () => void;
   children?: React.ReactNode;
 }
 
-const Modal = ({ show, children }: Showable) => {
-  return <>{show && <div className={styles.container}>{children}</div>}</>;
+const Modal = ({ show, children, onClose }: Showable) => {
+  if (!show) {
+    return null;
+  }
+  return (
+    <>
+      <div className={styles.overlay}></div>
+      <div className={styles.container}>
+        <div className={styles.topBar}>
+          <button className={styles.closeBtn} onClick={onClose}>
+            <BsX />
+            <span>Close Modal</span>
+          </button>
+        </div>
+        {children}
+      </div>
+    </>
+  );
 };
 
 export default Modal;
