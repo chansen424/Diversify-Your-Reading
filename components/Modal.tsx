e41672f68354cc26401c4c2d7cc6698f2ce05a57
Christopher Hansen <chansen424@yahoo.com>
2021-08-09 16:56:45 -0400

    feat: create a challenge modal

--- components/Modal.tsx
+++ components/Modal.tsx
@@ -4,10 +4,11 @@ import { BsX } from "react-icons/bs";
 interface Showable {
   show: boolean;
   onClose: () => void;
+  title?: string;
   children?: React.ReactNode;
 }
 
-const Modal = ({ show, children, onClose }: Showable) => {
+const Modal = ({ show, title, children, onClose }: Showable) => {
   if (!show) {
     return null;
   }
@@ -16,6 +17,7 @@ const Modal = ({ show, children, onClose }: Showable) => {
       <div className={styles.overlay}></div>
       <div className={styles.container}>
         <div className={styles.topBar}>
+          {title && <h2>{title}</h2>}
           <button className={styles.closeBtn} onClick={onClose}>
             <BsX />
             <span>Close Modal</span>
--- pages/challenges/index.tsx
+++ pages/challenges/index.tsx
@@ -5,7 +5,9 @@ import styles from "../../styles/Challenges.module.css";
 import { firestore, auth, logout } from "../../config";
 import { GetServerSideProps } from "next";
 import { useAuthState } from "react-firebase-hooks/auth";
-import { useEffect } from "react";
+import { FormEvent, useEffect, useState } from "react";
+import Modal from "../../components/Modal";
+import { BsPlusCircleFill } from "react-icons/bs";
 
 interface ChallengesProps {
   challenges: IChallenge[];
@@ -15,12 +17,29 @@ export default function Challenges({ challenges }: ChallengesProps) {
   const [user, loading, error] = useAuthState(auth);
   const router = useRouter();
 
+  const [modalOpen, setModalOpen] = useState(false);
+  const [createTitle, setCreateTitle] = useState("");
+  const [createDescription, setCreateDescription] = useState("");
+  const [goalValues, setGoalValues] = useState<{ [id: number]: string }>({});
+
   useEffect(() => {
     // Documentation said user === undefined if logged out, but apparently not
     if (!loading && user === null && router) {
       router.push("/");
     }
   }, [user, loading, router]);
+
+  const onCreateChallenge = (e: FormEvent<HTMLFormElement>) => {
+    e.preventDefault();
+    firestore.collection("challenges").add({
+      title: createTitle,
+      description: createDescription,
+      goals: Object.values(goalValues),
+    });
+    setCreateTitle("");
+    setCreateDescription("");
+    setGoalValues({});
+  };
 
   return (
     <div className={styles.container}>
@@ -30,15 +49,96 @@ export default function Challenges({ challenges }: ChallengesProps) {
         <link rel="icon" href="/favicon.ico" />
       </Head>
 
+      <Modal
+        show={modalOpen}
+        onClose={() => {
+          setModalOpen(false);
+          setCreateTitle("");
+          setCreateDescription("");
+          setGoalValues([]);
+        }}
+        title="Create A Challenge"
+      >
+        <form onSubmit={onCreateChallenge}>
+          <label htmlFor="create-title">Title</label>
+          <input
+            value={createTitle}
+            onChange={(e) => setCreateTitle(e.target.value)}
+            name="create-title"
+            id="create-title"
+            placeholder="Title"
+          />
+          <label htmlFor="create-desc">Description</label>
+          <input
+            value={createDescription}
+            onChange={(e) => setCreateDescription(e.target.value)}
+            id="create-desc"
+            placeholder="Description"
+          />
+          {Object.entries(goalValues).map((value) => {
+            const goalNumber = Number(value[0]);
+            const goalText = value[1];
+            return (
+              <div key={`goal-${goalNumber}`}>
+                <label
+                  htmlFor={`goal-${goalNumber}`}
+                >{`Goal #${goalNumber}`}</label>
+                <input
+                  onChange={(e) =>
+                    setGoalValues({
+                      ...goalValues,
+                      [goalNumber]: e.target.value,
+                    })
+                  }
+                  value={goalText}
+                  id={`goal-${goalNumber}`}
+                  placeholder={`Goal #${goalNumber}`}
+                />
+              </div>
+            );
+          })}
+          <button
+            className={styles.addGoalsBtn}
+            type="button"
+            onClick={() =>
+              setGoalValues({
+                ...goalValues,
+                [Object.keys(goalValues).length + 1]: "",
+              })
+            }
+          >
+            <BsPlusCircleFill />
+            <span>Add Goal</span>
+          </button>
+
+          <button
+            style={{ marginTop: "10px" }}
+            className={styles.basicBtn}
+            type="submit"
+          >
+            Submit
+          </button>
+        </form>
+      </Modal>
+
       <main className={styles.main}>
         <h1 className={styles.title}>Challenges</h1>
         <p>Logged in as {user?.displayName}</p>
-        <button
-          className={styles.logoutBtn}
-          onClick={() => logout(() => router.push("/"))}
-        >
-          Logout
-        </button>
+        <span>
+          <button
+            className={styles.basicBtn}
+            onClick={() => logout(() => router.push("/"))}
+          >
+            Logout
+          </button>
+
+          <button
+            className={styles.basicBtn}
+            onClick={() => setModalOpen(true)}
+          >
+            Create A Challenge
+          </button>
+        </span>
 
         {challenges.map((challenge) => (
           <Challenge key={challenge.id} {...challenge} />
